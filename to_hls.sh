51f334486c1b5ba3e1ebb3c01437a6f69e4c034d
Andrew Rabert <draje@nullsum.net>
2018-03-07 20:30:31 -0500

    wip

--- to_hls.sh
+++ to_hls.sh
@@ -2,6 +2,10 @@
 MEDIA_ID="$1"
 VARIANT_ID="$2"
 FILE="$3"
+SEGMENT_START_NUMBER=0
+OUTPUT_DIR=./transcode/"${MEDIA_ID}"
+
+mkdir -p "${OUTPUT_DIR}"
 
 ffmpeg \
     -noaccurate_seek -f matroska \
@@ -11,5 +15,6 @@ ffmpeg \
     -ac 2 -ab 384000  -f segment -max_delay 5000000 -avoid_negative_ts disabled \
     -map_metadata -1 -map_chapters -1 -start_at_zero -segment_time 6  \
     -individual_header_trailer 0 -break_non_keyframes 1 -segment_format mpegts \
-    -segment_list_type m3u8 -segment_start_number 0 \
-    -y "transcode/"${MEDIA_ID}"/$"{VARIANT_ID}"_%d.ts"
+    -segment_list_type m3u8 \
+    -segment_start_number "${SEGMENT_START_NUMBER}" \
+    -y "${OUTPUT_DIR}"/"${VARIANT_ID}_%d.ts"
